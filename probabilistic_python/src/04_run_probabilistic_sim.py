52a834c5ba1a1eae97adc9b5eab4ad9f650d4c8b
Emma Chelsvig <echelsvig@gmail.com>
2020-05-01 15:58:17 -0400

    updates to run sims with .rpt output file

--- probabilistic_python/src/04_run_probabilistic_sim.py
+++ probabilistic_python/src/04_run_probabilistic_sim.py
@@ -7,33 +7,31 @@
 # setup
 from pyswmm import Simulation
 import os, pandas
-import rpy2.robjects as ro
-from rpy2.robjects.packages import importr
-import pandas.rpy.common as com
+# specify location
+print(os.path.abspath(os.curdir))
+os.chdir("..")
+dir_path = os.path.abspath(os.curdir)
+print(dir_path)
 
-d = {'package.dependencies': 'package_dot_dependencies',
-     'package_dependencies': 'package_uscore_dependencies'}
-swmmr = importr('swmmr', robject_translations = d)
-
-
-swmmr.read_out()
+input_path = dir_path + r'\input\swmm\input_'
+print(input_path)
 
 # nsims
 nsims = 5
 
-# todo set up dataframe to receive the outputs from the simulations...below
+
 
 # run swmm, for each sim
 for i in range(1, nsims+1):
-    simfolder = r'C:\Users\echelsvi\git\chelsvig_urban_pesticides\probabilistic_python\input\swmm\input_' + str(i)
-    print(simfolder)
-    print(os.path.exists(simfolder))
+    sim_folder = input_path + str(i)
+    sim_file = os.path.join(sim_folder, "NPlesantCreek.inp")
 
-    simfile = os.path.join(simfolder, "NPlesantCreek" + "." + "inp")
-    print(simfile)
+    sim = Simulation(sim_file)
+    for step in sim:
+        pass
+    sim.report()
 
-    sim = Simulation(simfile)
-    sim.execute()
 
-    # todo grab the desired output variable of interest, and store in dataframe (created above loop)
+# todo set up dataframe to receive the outputs from the simulations
+# todo grab the desired output variable of interest, and store in dataframe
 
