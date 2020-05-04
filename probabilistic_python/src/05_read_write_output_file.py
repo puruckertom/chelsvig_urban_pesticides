5e57ba2acaee7dcb2b500dde845847333785e2ff
Emma Chelsvig <echelsvig@gmail.com>
2020-05-04 16:17:27 -0400

    additions to 05_ script

--- probabilistic_python/src/05_read_write_output_file.py
+++ probabilistic_python/src/05_read_write_output_file.py
@@ -23,6 +23,7 @@ firstday = date(2009, 1, 2)
 lastday = date(2009, 12, 31)
 delta = lastday - firstday
 days = delta.days
+dates = pandas.date_range(firstday, lastday).tolist()
 
 # read in the .rpt file for each simulation
 for rpt in range(1, nsims+1):
@@ -32,13 +33,15 @@ for rpt in range(1, nsims+1):
 
     # create blank data frame
     runf_df = pandas.DataFrame(data=None, index=None, columns=None, dtype=None, copy=False)
+    bif_df = pandas.DataFrame(data=None, index=None, columns=None, dtype=None, copy=False)
 
     # read the .rpt file
     file = open(file_path, "r")
 
     # for subcatchment 1
-    # create blank list to hold subcatchment's runf values
-    sub_list = []
+    # create blank list to hold subcatchment's runoff, and bifenthrin concentration
+    sub_list_runf = []
+    sub_list_bif = []
 
     # skip x lines to start at day1 for subcatchment1
     lines1 = file.readlines()[60:]
@@ -49,13 +52,18 @@ for rpt in range(1, nsims+1):
         fixline = " ".join(thisline.split())
         listline = fixline.split()
         runf = listline[4]
+        bif = listline[7]
 
         # insert into blank list
-        sub_list.append(runf)
+        sub_list_runf.append(runf)
+        sub_list_bif.append(bif)
 
     # insert list into data frame col
-    runf_df[1, ] = sub_list
+    runf_df[1, ] = sub_list_runf
     runf_df.rename(columns={runf_df.columns[0]: 'sub_1'}, inplace=True)
+
+    bif_df[1, ] = sub_list_bif
+    bif_df.rename(columns={bif_df.columns[0]: 'sub_1'}, inplace=True)
 
     # for subcatchment 2 - 113
     for sub in range(2, 114):
@@ -64,8 +72,9 @@ for rpt in range(1, nsims+1):
         skipto = (60 + ((days + 9) * (sub - 1))) - (sub - 2)
         lines = file.readlines()[skipto:]
 
-        # create blank list to hold subcatchment's runf values
-        sub_list = []
+        # create blank list to hold subcatchment's runoff and bifenthrin concentration
+        sub_list_runf = []
+        sub_list_bif = []
 
         for thisday in range(0, days):
             # grab the runf value
@@ -73,21 +82,31 @@ for rpt in range(1, nsims+1):
             fixline = " ".join(thisline.split())
             listline = fixline.split()
             runf = listline[4]
+            bif = listline[7]
 
             # insert into blank list
-            sub_list.append(runf)
+            sub_list_runf.append(runf)
+            sub_list_bif.append(bif)
 
         # insert list into data frame col
-        runf_df[sub, ] = sub_list
+        runf_df[sub, ] = sub_list_runf
+        bif_df[sub, ] = sub_list_bif
 
         # rename column names
         runf_df.rename(columns={runf_df.columns[sub - 1]: 'sub_' + str(sub)}, inplace=True)
+        bif_df.rename(columns={bif_df.columns[sub - 1]: 'sub_' + str(sub)}, inplace=True)
 
     # see what output looks like
     print(runf_df)
+    print(bif_df)
 
-# todo rename rows with date
-# todo add column that sums across the rows
+    # add a date column
+    runf_df['date'] = pandas.date_range(start='1/3/2009', periods=len(runf_df), freq='D')
+    bif_df['date'] = pandas.date_range(start='1/3/2009', periods=len(bif_df), freq='D')
+
+    # add a total sum column
+    runf_df["runf_sum"] = runf_df.sum(axis=1)
+    bif_df["bif_sum"] = bif_df.sum(axis=1)
+
 # todo write out the entire data frame
 # todo write out just the date and total_runf columns
-# todo do the same for the concentration of bifenthrin variable
