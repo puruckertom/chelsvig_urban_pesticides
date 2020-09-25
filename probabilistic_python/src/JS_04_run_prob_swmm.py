297d5bc5d31f154707742fe9271bc1b54064d49e
stelmanj <stelman@ucdavis.edu>
2020-09-25 11:04:57 -0700

    version of the 04 file that runs fewer, shorter simulations using JS_... files

---
+++ probabilistic_python/src/JS_04_run_prob_swmm.py
@@ -0,0 +1,36 @@
+# --------------------------------------------------
+# run probabilistic simulations, save output
+# --------------------------------------------------
+
+# setup
+from pyswmm import Simulation
+import os
+
+# nsims
+nsims = 1
+
+# specify location
+print(os.path.abspath(os.curdir))
+os.chdir("..")
+dir_path = os.path.abspath(os.curdir)
+print(dir_path)
+
+input_path = dir_path + r'\input\swmm\input_'
+print(input_path)
+
+# run swmm, for each sim
+for i in range(1, nsims+1):
+    sim_folder = input_path + str(i)
+    sim_file = os.path.join(sim_folder, "JS_NPlesantCreek.inp")
+
+    binary_path = sim_folder + r'\JS_NPlesantCreek.out'
+
+    # delete pre-existing .out in order to run swmm
+    if os.path.exists(binary_path):
+        os.remove(binary_path)
+    else:
+        print("Can not delete the file as it doesn't exist")
+
+    # load the model - no interaction, write out binary file
+    sim = Simulation(inputfile=sim_file, reportfile=None, outputfile=binary_path)
+    sim.execute()
