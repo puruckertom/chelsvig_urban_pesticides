025300e7d475294650bd09db95ff042e420fda74
Julia Stelman <stelman@ucdavis.edu>
2020-11-19 09:07:25 -0800

    added missing comma

--- src/Run_all.py
+++ src/Run_all.py
@@ -39,5 +39,5 @@ hours = total_seconds//3600
 minutes = (total_seconds%3600)//60
 seconds = round(total_seconds%60)
 
-print("Total Runtime:", hours, "hours," minutes, "minutes, and", seconds, "seconds.")
+print("Total Runtime:", hours, "hours,", minutes, "minutes, and", seconds, "seconds.")
 
