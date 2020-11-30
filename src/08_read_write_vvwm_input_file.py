2497bc153d6df19320d56467a1deec646d7fc1ba
Julia Stelman <stelman@ucdavis.edu>
2020-11-30 10:40:23 -0800

    file path construction

--- src/08_read_write_vvwm_input_file.py
+++ src/08_read_write_vvwm_input_file.py
@@ -7,15 +7,17 @@ import shutil, os, pandas as pd
 from path_names import dir_path, vvwm_path
 from prpy_bookkeeping import *
 
-outfalls = ['\outfall_31_26', '\outfall_31_28', '\outfall_31_29', '\outfall_31_35',
-            '\outfall_31_36', '\outfall_31_38', '\outfall_31_42']
+# outfalls = ['\outfall_31_26', '\outfall_31_28', '\outfall_31_29', '\outfall_31_35',
+#             '\outfall_31_36', '\outfall_31_38', '\outfall_31_42']
+outfalls = ['outfall_31_26', 'outfall_31_28', 'outfall_31_29', 'outfall_31_35',
+            'outfall_31_36', 'outfall_31_38', 'outfall_31_42']
 
 # nsims
 nsims = 5
 
 # read in lhs_sampled_params
 loginfo("Reading in lhs sampled parameters from <" + dir_path + "\io\lhs_sampled_params_vvwm.csv>.")
-lhs_design = pd.read_csv(dir_path+r'\io\lhs_sampled_params_vvwm.csv')
+lhs_design = pd.read_csv(os.path.join(dir_path, "io", "lhs_sampled_params_vvwm.csv"))#dir_path+r'\io\lhs_sampled_params_vvwm.csv')
 
 # round lhs decimals
 lhs_design = lhs_design.round(
@@ -29,14 +31,14 @@ loginfo("Looping thru outfalls for navigating to each vwmm folder and then each 
 for o in outfalls:
 
     # set pathways
-    outfall_dir = vvwm_path + o
+    outfall_dir = os.path.join(vvwm_path, o)#vvwm_path + o
 
     # do for each simulation...
     loginfo("Looping thru simulations of " + o[1:] + " to replace inputs with lhs inputs.")
     for Ite in range(1, nsims + 1):
 
         # create new input folder for sim
-        new_dir = outfall_dir + r'\input_' + str(Ite)
+        new_dir = os.path.join(outfall_dir, "input_" + str(Ite))#outfall_dir + r'\input_' + str(Ite)
         if not os.path.exists(new_dir):
             os.mkdir(new_dir)
             print("Folder ", Ite, " created", "\n")
@@ -44,8 +46,8 @@ for o in outfalls:
             print("Folder ", Ite, "already exists")
 
         # copy base file into new file location
-        old_path = vvwm_path + r'\vvwmTransfer.txt'
-        new_path = new_dir + r'\vvwmTransfer.txt'
+        old_path = os.path.join(vvwm_path, "vvwmTransfer.txt")#vvwm_path + r'\vvwmTransfer.txt'
+        new_path = os.path.join(new_dir, "vvwmTransfer.txt")#new_dir + r'\vvwmTransfer.txt'
         shutil.copyfile(old_path, new_path)
 
         # start reading the new file
