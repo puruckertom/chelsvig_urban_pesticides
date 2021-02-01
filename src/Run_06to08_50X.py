feec8654fc2338b5e73907de7b378e011f59538f
Julia Stelman <jstelman@atmos3.cm.cluster>
2021-02-01 10:59:02 -0500

    second half of 1-8 run on 50 sims instead of 1000

---
+++ src/Run_06to08_50X.py
@@ -0,0 +1,20 @@
+import time
+start = time.process_time()
+
+nsims = 50
+
+# run 6-8
+script = "06"
+svf06 = __import__('06_setup_vvwm_files')
+script = "07"
+lpv07 = __import__('07_lhs_param_vvwm')
+script = "08"
+rwvif08 = __import__('08_read_write_vvwm_input_file')
+
+# report runtime
+total_seconds = time.process_time() - start
+hours = total_seconds//3600
+minutes = (total_seconds%3600)//60
+seconds = round(total_seconds%60)
+
+print("Total Runtime:", hours, "hours,", minutes, "minutes, and", seconds, "seconds.")
